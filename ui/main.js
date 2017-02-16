ad2fb63fa45ecf6373500a5314d0e9a4fecaf972
Kartikey Mishra <kartikeym1212@gmail.com>
2017-02-16 23:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+
+
+var img=document.getElementById('madi');
+
+    img.onclick =function(){
+        
+        img.style.marginLeft ='100px';
+        
+        
+    };
+    
+    
+    
+    
