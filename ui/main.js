6595501322c200ef2e655561e3da8d085c4b48c6
Kartikey Mishra <kartikeym1212@gmail.com>
2017-03-03 01:11:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,2 @@
-console.log('Loaded!');
 
-
-
-
-//move the image
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-
-   function moveRight() 
-   {
-       marginLeft = marginLeft+2;
-       img.style.marginLeft = marginLeft+ 'px';
-       
-       
-   }
-    img.onclick =function(){
-        
-       var interval =setInterval(moveRight, 30);
-       
-        
-        
-    };
-    
-    
-    
     
