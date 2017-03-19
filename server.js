40a4dc8f33ca489fd824a0b69bc86d74de147093
Kartikey Mishra <kartikeym1212@gmail.com>
2017-03-19 21:50:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.get('/article-one', function(req,res){
 
 app.get('/article-two', function(req,res){
     
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send("Article two requsted and will be served here!"); 
+    
     
 });
 
