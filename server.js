4d9991bfa587d21905c1abe128a700a058d3825b
Kartikey Mishra <kartikeym1212@gmail.com>
2017-03-21 22:11:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,6 +33,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
+
+app.get('/ui/main.js', function(res,req){
+    
+    
+    
+     res.sendFile(path.join(__dirname, 'ui', 'main.js'));  
+    
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
