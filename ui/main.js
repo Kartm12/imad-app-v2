0ef7227c0bc29d0c9f5903f6e8eaae65904c8b4a
Kartikey Mishra <kartikeym1212@gmail.com>
2017-03-21 23:10:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,23 @@
+//counter code
 
+var button= document.getElementById("counter");
+
+
+button.onclick= function(){
+  
+  //make a request to the counter endpoint
+  
+  //Capture the response and store
+  
+  counter= counter+1;
+  var span = document.getElementById("count");
+  span.innerHTML=counter.toString();  
     
+    
+    
+    
+    
+    
+    
+};
+
